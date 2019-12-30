a78ad1fca5413097b19d496539d86768cd66c26f
hrzenger <zengh100@gmail.com>
2019-12-29 21:55:24 -0800

    Add to draw a circle which is filled with red color

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-import cv2
 import numpy as np
+import cv2
 img = np.zeros((512,512,3))
+
+# Center coordinates 
+center_coordinates = (120, 100) 
+  
+# Radius of circle 
+radius = 30
+   
+# Red color in BGR 
+color = (0, 0, 255) 
+   
+# Line thickness of -1 px 
+thickness = -1
+   
+# Using cv2.circle() method 
+# Draw a circle of red color of thickness -1 px 
+cv2.circle(img, center_coordinates, radius, color, thickness) 
 cv2.imshow("black image", img)
 
 key = cv2.waitKey(0) #0:  wait for infinitely
-
-#key = cv2.waitKey(3000)#pauses for 3 seconds before fetching next image
-#if key == 27:#if ESC is pressed, exit loop
-#    cv2.destroyAllWindows()
-#    break
 cv2.destroyAllWindows()
