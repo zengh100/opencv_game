5352d5b31ecdc4e306d31a7667eeaf55c826eb10
hrzenger <zengh100@gmail.com>
2019-12-29 22:13:03 -0800

    use while loop to draw moveing red ball; problem: the previous red ball is not erased.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import cv2
 img = np.zeros((512,512,3))
 
 # Center coordinates 
-center_coordinates = (120, 100) 
+center_x = 120
+center_y = 100 
+
   
 # Radius of circle 
 radius = 30
@@ -13,11 +15,15 @@ color = (0, 0, 255)
    
 # Line thickness of -1 px 
 thickness = -1
-   
-# Using cv2.circle() method 
-# Draw a circle of red color of thickness -1 px 
-cv2.circle(img, center_coordinates, radius, color, thickness) 
-cv2.imshow("black image", img)
 
-key = cv2.waitKey(0) #0:  wait for infinitely
+while(1):   
+    # Using cv2.circle() method 
+    # Draw a circle of red color of thickness -1 px 
+    center_x += 1
+    center_y += 1 
+    cv2.circle(img, (center_x, center_y), radius, color, thickness) 
+    cv2.imshow("black image", img)
+    key = cv2.waitKey(33) #0:  wait for infinitely
+    if(key == 27): 
+        break
 cv2.destroyAllWindows()
